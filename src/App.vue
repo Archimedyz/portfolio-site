072079aba8da9a6ff7bf160ac2b0372bf325f6de
awais.ali <awais.ali1248@gmail.com>
2025-02-03 21:01:56 -0500

    Setup minimal working site w/ Vue + Vite.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        Hey, welcome to my portfolio!
+    </header>
+
+    <main>
+        Loading content . . .
+    </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
