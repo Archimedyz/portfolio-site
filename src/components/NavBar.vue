a18580b219cc95fb910b6e266f6a321da58f4954
awais.ali <awais.ali1248@gmail.com>
2025-02-07 22:09:44 -0500

    Add styling - global + navbar.
    Replaced font for nav bar items.
    Added new font for nav-logo.
    Set global font - Poppins.
    Updated navbar styling.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,13 +3,15 @@
 
 <template>
 <div id="nav-bar">
-    <div id="nav-logo">Awais Ali</div>
-    <div id="nav-links">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#experience">Experience</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
+    <div id="nav-content">
+        <div id="nav-logo">Awais Ali</div>
+        <div id="nav-links">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#experience">Experience</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+        </div>
     </div>
 </div>
 </template>
@@ -24,19 +26,34 @@
         background-color: var(--primary)
     }
 
+    #nav-content {
+        position: relative;
+        height: 100%
+    }
+
+    #nav-content > div {
+        position: absolute;
+        bottom: 5px;
+    }
+
     #nav-logo {
-        float: left;
-        height: 100%;
+        font-family: "Playwrite GB S", serif;
+        font-style: italic;
+        font-size: 2rem;
+        display: inline-flex;
+        left: 50px;
+        color: var(--secondary);
     }
 
     #nav-links {
-        float: right;
-        position: relative;
-        bottom: 0px;
+        display: inline-flex;
+        right: 20px;
     }
 
-    #navlinks a {
-        font-size: 1.5em;
-        font-family: "Bungee Shade", serif;
+    #nav-links a {
+        font-size: 1.5rem;
+        padding-right: 15px;
+        text-decoration: none;
+        color: var(--secondary);
     }
 </style>
