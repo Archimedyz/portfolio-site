804c2b2cf8c3eb112e73bf16bbc1d03d77aac702
awais.ali <awais.ali1248@gmail.com>
2025-02-19 19:32:16 -0500

    Update pallete. Add hover effects on navbar.

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,7 +6,7 @@
 <div id="about">
     <div class="about-content">
         <h1>About Me</h1>
-        <p>Hi, I'm Awais, a backend developer located in beautiful Montreal, Canada. 🇨🇦</p>
+        <p>Hi, I'm Awais, a backend developer located in beautiful Montreal, Canada.</p>
         <p>I thrive on creating the behind-the-scenes magic that powers applications. My core expertise lies in C#, .NET Core, MS SQL, and Git. I'm passionate about writing efficient code and improving application performance. Although backend is my primary domain, I'm not afraid to venture into the frontend world (as you can see from this portfolio!).</p>
         <p>I am always open for collaboration and am eager to work with fellow developers on a variety of projects, including open-source initiatives and even small indie game development.</p>
         <p>When I'm not coding, I enjoy unwinding with chess, manga, and podcasts.</p>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,44 @@
 <script setup>
+import { ref } from 'vue';
+
+const onHover = ref(false);
+
+const navItems = ref([
+    { anchor: "#home", text: "Home", additionalClass: "" },
+    { anchor: "#about", text: "About", additionalClass: "" },
+    { anchor: "#experience", text: "Experience", additionalClass: "" },
+    { anchor: "#projects", text: "Projects", additionalClass: "" },
+    { anchor: "#contact", text: "Contact", additionalClass: "" }
+]);
+
+const logoAdditionalClass = ref("");
+
+function hoverOn(item)
+{
+    item.additionalClass = "hovered";
+}
+
+function hoverOff(item)
+{
+    item.additionalClass = "";
+}
 </script>
 
 <template>
-<div id="nav-bar">
-    <div id="nav-content">
-        <div id="nav-logo">Awais Ali</div>
-        <div id="nav-links">
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#experience">Experience</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
+<div class="nav-bar">
+    <div class="nav-content">
+        <div class="nav-logo">
+            <a href="#" @mouseover="logoAdditionalClass='hoveredLogo'" @mouseleave="logoAdditionalClass=''" class="nav-logo-link" :class="logoAdditionalClass">Awais Ali</a>
+        </div>
+        <div class="nav-links">
+            <a v-for="item in navItems" :href="item.anchor" @mouseover="hoverOn(item)" @mouseleave="hoverOff(item)" class="nav-link" :class="item.additionalClass">{{ item.text }}</a>
         </div>
     </div>
 </div>
 </template>
 
 <style scoped>
-    #nav-bar {
+    .nav-bar {
         position: sticky;
         top: 0px;
         left: 0px;
@@ -27,34 +48,50 @@
         z-index: 1;
     }
 
-    #nav-content {
+    .nav-content {
         position: relative;
         height: 100%
     }
 
-    #nav-content > div {
+    .nav-content > div {
         position: absolute;
         bottom: 5px;
     }
 
-    #nav-logo {
+    .nav-logo {
+        display: inline-flex;
+        left: 50px;
+    }
+
+    .nav-logo-link {
         font-family: "Playwrite GB S", serif;
         font-style: italic;
         font-size: 2rem;
-        display: inline-flex;
-        left: 50px;
-        color: var(--secondary);
+        text-decoration: none;
+        color: var(--primary-light);
+        cursor: pointer;
     }
 
-    #nav-links {
+    .nav-links {
         display: inline-flex;
         right: 20px;
     }
 
-    #nav-links a {
+    .nav-link {
+        vertical-align: top;
         font-size: 1.5rem;
-        padding-right: 15px;
+        margin-right: 15px;
         text-decoration: none;
-        color: var(--secondary);
+        color: var(--primary-light);
+        border-bottom: 2px solid transparent;
+    }
+
+    .hovered {
+        border-bottom-color: var(--light);
+        color: var(--light);
+    }
+
+    .hoveredLogo {
+        color: var(--light);
     }
 </style>
