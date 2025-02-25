63b9f7c3f737957c985842ef891a682cdcc8f584
awais.ali <awais.ali1248@gmail.com>
2025-02-24 22:57:02 -0500

    Add content to Projects section.
    Add image for portfolio site project.
    Add image for open link icon.
    Add component ProjCard to display different projects.
    Add one project to Projects section.

---
+++ src/components/ProjCard.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps([
+    'title', 'description', 'projectTech', 'imageFileName', 'link'
+])
+
+</script>
+
+<template>
+    <div class='project-card'>
+        <div class="project-img">
+            <img :src="'/assets/' + props.imageFileName" alt="project image"/>
+        </div>
+        <div class="project-info-div">
+            <div class="project-title-div">
+                <h3 class="project-title">{{ props.title }}</h3>
+                <a :href="props.link">
+                    <img 
+                        class="go-to-img" 
+                        src="/assets/icons/open-link.png"
+                        alt="go to project"
+                    />
+                </a>
+            </div>
+            <div class="project-technologies">
+                <span v-for="(tech, i) in props.projectTech" class="tech-item">
+                    {{ (i > 0 ? '- ' : '') + tech }}
+                </span>
+            </div>
+            <div class="project-description">
+                <p class="description">
+                    {{ props.description }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.project-card {
+    border-radius: 10px;
+    border: 1px solid black;
+    box-shadow: var(--color-dark) 2px 2px; 
+}
+
+.project-img {
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+    width: 100%;
+}
+
+.project-img > img {
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+    width: 100%;
+}
+
+.project-info-div {
+    padding: 5px 10px;
+}
+
+.project-title-div{
+    width: 100%;
+    display: block;
+}
+
+.project-title {
+    display: inline-block;
+    vertical-align: middle;
+    margin: 5px 5px 5px 0;
+}
+
+.go-to-img {
+    height: 24px;
+    vertical-align: middle;
+    display: inline-block;
+}
+
+.tech-item {
+    margin-right: 4px;
+    font-size: 0.8rem;
+    font-weight: bold;
+}
+
+
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,11 +1,20 @@
 <script setup>
-
+import ProjCard from './ProjCard.vue';
 </script>
 
 <template>
 <div id="projects">
     <div class="section-content">
         <h1>Projects</h1>
+        <div class="project-cards">
+            <ProjCard
+                title="Portfolio Website"
+                description="A portfolio site designed with Vue. You're looking at it right now!"
+                :projectTech="['Vue', 'JS', 'CSS', 'HTML']"
+                imageFileName="proj-portfolio-site.png"
+                link="/"
+            />
+        </div>
     </div>
 </div>
 </template>
@@ -14,5 +23,10 @@
 #projects {
     background-color: var(--color-quaternary);
     width: 100%;
+}
+
+.project-cards > * {
+    max-width: 45%;
+    margin-right: 10%;
 }
 </style>
