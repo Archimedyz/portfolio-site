54ef99e74d678a23e20c32226ee7e2e58bef80c6
awais.ali <awais.ali1248@gmail.com>
2025-04-09 18:36:38 -0400

    Add version info to footer.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import { ref } from 'vue';
 import NavBar from './components/NavBar.vue'
 import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Experience from './components/Experience.vue'
 import Projects from './components/Projects.vue'
 import Contact from './components/Contact.vue'
+
+const commitHash = ref(__COMMIT_HASH__);
 </script>
 
 <template>
@@ -17,11 +20,19 @@ import Contact from './components/Contact.vue'
         <Contact />
     </main>
     <footer>
+        <i>portfolio-site_{{ commitHash }}</i>
     </footer>
 </template>
 
 <style scoped>
 main > * {
     display: inline-block;
+}
+
+footer {
+    text-align: center;
+    background-color: var(--color-dark);
+    color: var(--color-mid);
+    font-size: 10px;
 }
 </style>
