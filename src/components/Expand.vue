dcddf385ab0e20f77e49c9d66ba303ef2f37972b
awais.ali <awais.ali1248@gmail.com>
2025-04-14 23:25:12 -0400

    Remove old logic for Expand.

--- src/components/Expand.vue
+++ src/components/Expand.vue
@@ -6,42 +6,6 @@ import { debounce } from 'lodash';
 var props = defineProps([
     'show'
 ]);
-
-const expandRef = useTemplateRef('expand');
-
-const expandHeight = ref(0);
-
-const updateExpandHeightOnNextFrame = () => {
-  requestAnimationFrame(() => {
-    if (expandRef.value) {
-      // Force a layout recalculation
-      expandRef.value.style.display = 'block';
-      expandHeight.value = expandRef.value.offsetHeight;
-      expandRef.value.style.display = '';
-    }
-    else {
-      expandHeight.value = 0;
-    }
-
-    addTransitions.value = false;
-  });
-};
-
-const addTransitions = ref(false);
-
-onMounted(() => {
-    const debouncedResizeUpdate = debounce(updateExpandHeightOnNextFrame, 100);
-
-    // update values after resizing, as it may affect content height
-    window.addEventListener('resize', debouncedResizeUpdate);
-
-    updateExpandHeightOnNextFrame();
-});
-
-onUpdated(() => {
-    // only add transition CSS after the first update
-    addTransitions.value = true;
-});
 </script>
 
 <template>
@@ -65,17 +29,5 @@ onUpdated(() => {
 .expand-container {
     /* This is done to (somewhat) offset Naive-UI's baked in padding. */
     margin-top: -16px;
-}
-
-.expand {
-    margin-top: v-bind(-expandHeight);
-}
-
-.expand-show {
-    margin-top: 0px;
-}
-
-.expand-transition {
-    transition: margin-top 0.6s ease-in;
 }
 </style>
