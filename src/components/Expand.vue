70aca5750bdec09d505d948e63914c38932206e3
awais.ali <awais.ali1248@gmail.com>
2025-04-14 23:22:55 -0400

    Update Expand implementation to use Naive-UI.

--- src/components/Expand.vue
+++ src/components/Expand.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, useTemplateRef, onMounted, onUpdated } from 'vue';
+import { NCollapse, NCollapseItem, NIcon } from 'naive-ui';
 import { debounce } from 'lodash';
 
 var props = defineProps([
@@ -45,22 +46,25 @@ onUpdated(() => {
 
 <template>
     <div class="expand-container">
-        <div
-            ref="expand"
-            class="expand"
-            :class="{
-                'expand-show': props.show,
-                'expand-transition': addTransitions
-            }"
+        <n-collapse
+            default-expanded-names=""
+            :expanded-names="show ? ['expand-content'] : []"
+            display-directive="show"
         >
-            <slot />
+            <template #arrow>
+                <n-icon style="height: 0;"></n-icon>
+            </template>
+            <n-collapse-item name="expand-content">
+                <slot />
+            </n-collapse-item>
+        </n-collapse>
         </div>
-    </div>
 </template>
 
 <style scoped>
 .expand-container {
-    overflow-y: hidden;
+    /* This is done to (somewhat) offset Naive-UI's baked in padding. */
+    margin-top: -16px;
 }
 
 .expand {
